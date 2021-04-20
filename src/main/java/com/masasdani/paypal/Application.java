de5d1d58192b11cffdcce387a03103a48b3027a1
fo0 <fo0@fo0.me>
2021-04-20 20:07:13 +0000

    updated code

--- src/main/java/com/masasdani/paypal/Application.java
+++ src/main/java/com/masasdani/paypal/Application.java
@@ -1,7 +1,7 @@
 package com.masasdani.paypal;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +11,11 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+            // disable default petstore
+            .properties("springdoc.swagger-ui.disable-swagger-default-url=true") 
+            .properties("springdoc.api-docs.enabled=false")
+            .properties("server.forward-headers-strategy=framework")
+            .run(args);
 	}
 }
--- src/main/java/com/masasdani/paypal/controller/PaymentController.java
+++ src/main/java/com/masasdani/paypal/controller/PaymentController.java
@@ -2,14 +2,6 @@ package com.masasdani.paypal.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.masasdani.paypal.config.PaypalPaymentIntent;
 import com.masasdani.paypal.config.PaypalPaymentMethod;
 import com.masasdani.paypal.service.PaypalService;
@@ -18,31 +10,39 @@ import com.paypal.api.payments.Links;
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.log4j.Log4j2;
+
 @Controller
 @RequestMapping("/")
+@Log4j2
 public class PaymentController {
 	
 	public static final String PAYPAL_SUCCESS_URL = "pay/success";
 	public static final String PAYPAL_CANCEL_URL = "pay/cancel";
 	
-	private Logger log = LoggerFactory.getLogger(getClass());
-	
 	@Autowired
 	private PaypalService paypalService;
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public String index(){
 		return "index";
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "pay")
+	@PostMapping(value = "pay")
 	public String pay(HttpServletRequest request){
 		String cancelUrl = URLUtils.getBaseURl(request) + "/" + PAYPAL_CANCEL_URL;
 		String successUrl = URLUtils.getBaseURl(request) + "/" + PAYPAL_SUCCESS_URL;
 		try {
 			Payment payment = paypalService.createPayment(
-					4.00, 
-					"USD", 
+					1.00, 
+					"EUR", 
 					PaypalPaymentMethod.paypal, 
 					PaypalPaymentIntent.sale,
 					"payment description", 
@@ -59,12 +59,12 @@ public class PaymentController {
 		return "redirect:/";
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = PAYPAL_CANCEL_URL)
+	@GetMapping(value = PAYPAL_CANCEL_URL)
 	public String cancelPay(){
 		return "cancel";
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = PAYPAL_SUCCESS_URL)
+	@GetMapping(value = PAYPAL_SUCCESS_URL)
 	public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId){
 		try {
 			Payment payment = paypalService.executePayment(paymentId, payerId);
